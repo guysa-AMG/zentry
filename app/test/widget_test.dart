ff64566f2d4515b6c47b1e52385fc0b75867c3e9
Guysa <gumumjhb025@gmail.com>
2026-05-09 16:44:11 +0200

    note

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -4,6 +4,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
+  
     await tester.pumpWidget(const ProviderScope(child: ZentryApp()));
     expect(find.text('DRIVING MODE'), findsOneWidget);
   });
